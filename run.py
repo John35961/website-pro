8a3940466271bfeac8ac4e57b6b01826926fd1b0
John35961 <jguillet@mytraffic.fr>
2019-08-20 20:22:32 +0200

    Fix bilingual mode

--- run.py
+++ run.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('layout.html')
 
-@app.route("/fr")
-def home_fr():
-    return render_template('fr.html')
-
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
