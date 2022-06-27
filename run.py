0721d9013d34a9e546e450d403510d41f8c17f91
Jean Guillet <jean.guillet1@gmail.com>
2022-06-27 20:17:48 +0200

    replaced home content in folder

--- run.py
+++ run.py
@@ -9,17 +9,20 @@ def home():
 
 @app.route("/en/")
 def en():
-    with open('content/en.yml', 'r') as file:
+    with open('content/home/en-home.yml', 'r') as file:
         yml = yaml.safe_load(file)
 
-    return render_template('EN_layout.html', yml=yml)
+    with open('content/projects.yml', 'r') as file:
+        proj = yaml.safe_load(file)
+
+    return render_template('EN_layout.html', yml=yml, proj=proj)
 
 @app.route("/fr/")
 def fr():
-    with open('content/fr.yml', 'r') as file:
+    with open('content/home/fr-home.yml', 'r') as file:
         yml = yaml.safe_load(file)
 
     return render_template('FR_layout.html', yml=yml)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
