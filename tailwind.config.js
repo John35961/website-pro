c16eb99d1f28a816cf3fc35093c9b81a7bdac280
Jean Guillet <jean.guillet1@gmail.com>
2022-06-28 11:13:47 +0200

    tailwindcss first setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/base.html',
+            './templates/home/*.html',
+            './templates/partials/*.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
